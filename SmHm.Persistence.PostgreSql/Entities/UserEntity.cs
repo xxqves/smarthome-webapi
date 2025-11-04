8ce24d38bf2ba9257bcc93b79da6dc64cf81d2b2
Danil <Danil@DESKTOP-ETI2JS2>
2025-11-05 00:53:57 +0300

    add user entity

---
+++ SmHm.Persistence.PostgreSql/Entities/UserEntity.cs
@@ -0,0 +1,13 @@
+﻿namespace SmHm.Persistence.PostgreSql.Entities
+{
+    public class UserEntity
+    {
+        public Guid Id { get; set; }
+
+        public string UserName { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public string PasswordHash { get; set; } = string.Empty;
+    }
+}

4e66025af969e337a8c26e533e91f8262e549feb
Danil <Danil@DESKTOP-ETI2JS2>
2025-11-05 00:39:59 +0300

    add user domain model

---
+++ SmHm.Core/Models/User.cs
@@ -0,0 +1,33 @@
+﻿namespace SmHm.Core.Models
+{
+    public class User
+    {
+        public const int MAX_USERNAME_LENGTH = 12;
+
+        private User(Guid id, string userName, string email, string passwordHash)
+        {
+            Id = id;
+            UserName = userName;
+            Email = email;
+            PasswordHash = passwordHash;
+        }
+
+        public Guid Id { get; }
+
+        public string UserName { get; } = string.Empty;
+
+        public string Email { get; } = string.Empty;
+
+        public string PasswordHash { get; } = string.Empty;
+
+        public static User Create(Guid id, string userName, string email, string passwordHash)
+        {
+            if (userName.Length > MAX_USERNAME_LENGTH)
+            {
+                throw new ArgumentException("Name length can not be more than 12 characters");
+            }
+
+            return new User(id, userName, email, passwordHash);
+        }
+    }
+}
